c25c3afcb0f0d8c361b511a16772d9ee0c0c65fe
34jhoel <122816466+34jhoel@users.noreply.github.com>
2023-03-28 04:59:42 -0600

    Add files via upload

---
+++ Tareas/Introducción a la programación/Ejercicios tema 3/PrimeraParte.java
@@ -0,0 +1,12 @@
+public class PrimeraParte {
+
+  public static int sum(int num1, int num2, int num3) {
+    return num1 + num2 + num3;
+  }
+
+  public static void main(String[] args) {
+    int result = sum(2, 4, 6);
+    System.out.println("La suma de los tres números es: " + result);
+  }
+
+}
